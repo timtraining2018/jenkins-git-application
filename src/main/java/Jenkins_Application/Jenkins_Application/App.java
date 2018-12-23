f5ce300e8860c98d40498a42740c201b23fd8d9b
Timothy Ndunguru <timothy.ndunguru@egainternal.go.tz>
2018-12-23 15:44:09 +0300

    add new change

---
+++ src/main/java/Jenkins_Application/Jenkins_Application/App.java
@@ -0,0 +1,13 @@
+package Jenkins_Application.Jenkins_Application;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
